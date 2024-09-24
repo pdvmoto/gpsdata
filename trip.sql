f3326416336ef1912141b44309ce14d65f0434ea
pdvmoto <piet@basetide.com>
2024-09-24 13:25:35 +0200

    example of kml elements

--- trip.sql
+++ trip.sql
@@ -8,7 +8,7 @@ select id
 , substr ( trp_name, instr ( trp_name, '_' )+1, length ( trp_name) ) as trpnm
  from trip ; 
 
-
+-- min/max of trip, add to trip-info
 select t.id
 , substr ( trp_name, 1, instr ( trp_name, '_')-1 ) as trpnr 
 , substr ( t.trp_name, instr ( t.trp_name, '_' )+1, length ( t.trp_name) ) as trpnm
@@ -21,10 +21,31 @@ where 1=1
   and tp.gfil_id = l.gfil_id 
   and tp.line_nr = l.line_nr
   and tp.trp_id  = t.id
+  and trp_name like '137_OUG%2016'
 group by 
   t.id
 , t.trp_name
 , substr ( t.trp_name, instr ( t.trp_name, '_' )+1, length ( t.trp_name) ) 
 order by 5 
+;
+
+-- points, coordinates of trip
+-- optional: draw 1 line per hour, with placemarks at start of hr ?
+select t.id
+, substr ( trp_name, 1, instr ( trp_name, '_')-1 ) as trpnr 
+, substr ( t.trp_name, instr ( t.trp_name, '_' )+1, length ( t.trp_name) ) as trpnm
+-- , count (*) nr_lines
+-- , min (dt) earliest, max ( dt) latest 
+, to_char ( l.dt, 'YYYY MON DD HH24:MI:SS' ) 
+, lat, lon
+from trip t 
+   , gps_line l
+   , trip_point tp
+where 1=1
+  and tp.gfil_id = l.gfil_id 
+  and tp.line_nr = l.line_nr
+  and tp.trp_id  = t.id
+  and trp_name like '137_OUG%2016'
+order by l.dt  
 ;
 
